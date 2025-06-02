2ad8c5b1cac0435db2bb57d4932fa4238f9d6bae
Korolev Roman Igorevich <141877654+MentalMentos@users.noreply.github.com>
2025-06-02 10:57:21 +0300

    add files

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func f(n int) int {
+	res := 0
+	for n > 0 {
+		res *= 10
+		res += n % 10
+		n /= 10
+	}
+	return res
+}
+
+func main() {
+	mp := make(map[float64]struct{})
+	for i := 2; i < 1030; i++ {
+		fn := f(i)
+		ffn := f(fn)
+		mp[float64(ffn)/float64(i)] = struct{}{}
+	}
+	fmt.Println("Ответ:", len(mp))
+}
